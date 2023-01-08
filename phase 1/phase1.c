6d6beeefb817b0b2d24eafa4ac929f08532baab0
arefzarezadeh <arefzarezade@gmail.com>
2023-01-08 19:57:55 +0330

    added 'removestr'

--- phase 1/phase1.c
+++ phase 1/phase1.c
@@ -1,4 +1,43 @@
 #include<stdio.h>
+#include<stdbool.h>
+
+struct pos
+{
+    int line;
+    int position;
+};
+
+int getPos(char *address, struct pos p)
+{
+    int position = 0, i = 1, j = 0;
+    FILE *read = fopen(address, "r");
+    while (1)
+    {
+        if (i == p.line)
+        {
+            while (1)
+            {
+                if (j == p.position)
+                    break;
+                char c = fgetc(read);
+                if (c == EOF)
+                    break;
+                if (c == '\n')
+                    break;
+                j++;
+                position++;
+            }
+            break;
+        }
+        char c = fgetc(read);
+        if (c == EOF)
+            break;
+        if (c == '\n')
+            i++;
+        position++;
+    }
+    return position;
+}
 
 void createNewFile(char *address)
 {
@@ -25,18 +64,18 @@ void copyFile(char *fromAdress, char *toAdress)
     return;
 }
 
-void insert(char *address, int line, int pos, char *text)
+void insert(char *address, struct pos p, char *text)
 {
     FILE *r = fopen(address, "r");
     FILE *w = fopen("tmp.txt", "w");
     int i = 1, j = 0;
     while (1)
     {
-        if (i == line)
+        if (i == p.line)
         {
             while (1)
             {
-                if (j == pos)
+                if (j == p.position)
                     fputs(text, w);
                 char c = fgetc(r);
                 if (c == EOF)
@@ -77,12 +116,36 @@ void cat(char *address)
     return;
 }
 
+void removestr(char *address, struct pos p, int size, bool forward)
+{
+    int position = getPos(address, p), k = 0;
+    FILE *read = fopen(address, "r");
+    FILE *tmp = fopen("tmp.txt", "w");
+
+    while (1)
+    {
+        char c = fgetc(read);
+        if (c == EOF)
+            break;
+        if (forward && !(k >= position && k < position + size))
+            fputc(c, tmp);
+        else if ((!forward) && !(k < position && k >= position - size))
+            fputc(c, tmp);
+        k++;
+    }
+
+    fclose(read);
+    fclose(tmp);
+    copyFile("tmp.txt", address);
+    return;
+}
 
 int main()
 {
     createNewFile("test.txt");
-    insert("test.txt", 1, 0, "Hello\nHow are you?");
-    cat("test.txt");
-    cat("test.txt");
+    struct pos start = {1, 0};
+    insert("test.txt", start, "Salam\nKhobi?");
+    struct pos ran = {2, 1};
+    removestr("test.txt", ran, 3, 0);
     return 17;
 }
