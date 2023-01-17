52ae8760fef5053a49f4ee80d656379e85e4e6b5
arefzarezadeh <arefzarezade@gmail.com>
2023-01-17 22:29:05 +0330

    added 'find'

---
+++ phase 1/find.h
@@ -0,0 +1,327 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define CAPACITY 255
+
+struct linkedList
+{
+    int value;
+    struct linkedList *next;
+};
+
+struct linkedList* create_linkedlist()
+{
+    struct linkedList *first = (struct linkedList *) malloc(sizeof(struct linkedList));
+    first->value = -10;
+    first->next = NULL;
+    return first;
+}
+
+void addLinkedList(struct linkedList* l, int val)
+{
+    struct linkedList* current = l;
+    while (current->value != -10)
+    {
+        if (current->next == NULL)
+        {
+            current->next = create_linkedlist();
+            current = current->next;
+        }
+        else
+            current = current->next;
+    }
+    current->value = val;
+    return;
+}
+
+void printLinkedList(struct linkedList *list)
+{
+    struct linkedList *check = list;
+    if (check->value == -10)
+        return;
+    printf("%d", check->value);
+    if (check->next == NULL)
+        return;
+    check = check->next;
+    while (check->value != -10)
+    {
+        printf(", %d", check->value);
+        if (check->next == NULL)
+            break;
+        check = check->next;
+    }
+    printf("\n");
+    return;
+}
+
+long sizeofFile(FILE *f)
+{
+    long current = ftell(f);
+    fseek(f, 0, SEEK_END);
+    long end = ftell(f);
+    long size = end - current;
+    fseek(f, -size, SEEK_CUR);
+
+    return size;
+}
+
+int sizeString(char *text)
+{
+    int x = 0;
+    while (1)
+    {
+        if (text[x] == '\0' || text[x] == EOF)
+            return x;
+        x++;
+    }
+}
+
+char * correctStar(char *text)
+{
+    char *replacement = (char *) malloc(sizeof(char) * (sizeString(text) + 1));
+    int pos = 0;
+    for (int i = 0; i < sizeString(text); i++)
+    {
+        if (text[i] == '\\' && text[i + 1] == '*')
+        {
+            replacement[pos] = '*';
+            i++;
+        }
+        else
+            replacement[pos] = text[i];
+        pos++;
+    }
+    replacement[pos] = '\0';
+    return replacement;
+}
+
+char * getWord(char *text, int index)
+{
+    int i = 0;
+    int textPos = 0;
+    char *word = (char *) malloc(CAPACITY * sizeof(char));
+    word[0] = '\0';
+    while (1)
+    {
+        //---------------ignore all the spaces before the word
+        while (text[textPos] == ' ' || text[textPos] == '\n')
+            textPos++;
+        i++;
+        //---------------end of that
+
+        //---------------adding the word to the string
+        if (i == index)
+        {
+            int position = 0;
+            while (text[textPos] != ' ' && text[textPos] != '\0' && text[textPos] != '\n' && text[textPos] != EOF)
+                word[position++] = text[textPos++];
+            textPos++;
+            word[position] = '\0';
+            break;
+        }
+        //---------------finished adding it
+
+        //---------------ignoring privious words
+        while (text[textPos] != ' ' && text[textPos] != '\0' && text[textPos] != '\n' && text[textPos] != EOF)
+            textPos++;
+
+        //---------------checking if the string has ended
+        if (text[textPos] == EOF || text[textPos] == '\0')
+            break;
+
+        textPos++;
+    }
+    return word;
+}
+
+char * fgetword(FILE *r)
+{
+    //-----------------ignore all spaces before the word
+    char c = fgetc(r);
+    while (c == ' ' || c == '\n')
+        c = fgetc(r);
+    //-----------------end of that
+
+    if (c == EOF)
+        return NULL;
+    char *word = (char *) calloc(CAPACITY, sizeof(char));
+
+    //-----------------get the word
+    int i = 0;
+    while (c != ' ' && c != '\n' && c != '\0' && c != EOF && i < CAPACITY)
+    {
+        word[i++] = c;
+        c = fgetc(r);
+    }
+    word[i] = '\0';
+    //-----------------end of that
+
+    return word;
+}
+
+int fcountWords(FILE *f)
+{
+    long current = ftell(f);
+    int i = 0;
+    while(fgetword(f) != NULL)
+        i++;
+    fseek(f, current, SEEK_SET);
+
+    return i;
+}
+
+int countWords(char *text)
+{
+    int i = 0;
+    while (*getWord(text, ++i) != '\0');
+    return i - 1;
+}
+
+bool wordcmp(char *realWord, char *text)
+{
+    int size = sizeString(text);
+    bool startWild = (text[0] == '*');
+    bool endWild = (text[size - 1] == '*' && text[size - 2] != '\\');
+    char *correctedText = correctStar(text);
+    size = sizeString(correctedText);
+
+    if (!endWild && !startWild) //-------------------------------------case 1
+        return !strcmp(realWord, correctedText);
+
+    if (!startWild && endWild) //--------------------------------------case 2
+    {
+        if (sizeString(realWord) < size - 1)
+            return false;
+        for (int i = 0; i < size - 1; i++)
+        {
+            if (realWord[i] != correctedText[i])
+                return false;
+        }
+        return true;
+    }
+
+    if (startWild && !endWild) //--------------------------------------case 3
+    {
+        int wordSize = sizeString(realWord);
+        if (wordSize < size - 1)
+            return false;
+        for (int i = 1; i < size; i++)
+        {
+            if (realWord[wordSize - i] != correctedText[size - i])
+                return false;
+        }
+        return true;
+    }
+
+    if (startWild && endWild) //---------------------------------------case 4
+    {
+        int wordSize = sizeString(realWord);
+        if (wordSize < size - 2)
+            return false;
+        for (int i = 0; i < wordSize - size + 3; i++)
+        {
+            bool isSimilar = true;
+            for (int pos = 1; pos < size - 1; pos++)
+            {
+                if (realWord[i + pos - 1] != correctedText[pos])
+                {
+                    isSimilar = false;
+                    break;
+                }
+            }
+            if (isSimilar)
+                return true;
+        }
+        return false;
+    }
+}
+
+long normalFind(FILE *r, char *textToBeFound, bool byword)
+{
+    long size = sizeofFile(r);
+    long current = ftell(r);
+
+    int numberOfWords = fcountWords(r);
+
+    int words;
+
+    if (byword)
+    {
+        fseek(r, 0, SEEK_SET);
+        int totalWords = fcountWords(r);
+        fseek(r, current, SEEK_SET);
+        words = totalWords - numberOfWords;
+    }
+
+    for (int i = 1; i <= numberOfWords - countWords(textToBeFound) + 1; i++)
+    {
+        int wSize = sizeString(fgetword(r));
+        fseek(r, -wSize - 1, SEEK_CUR);
+        int position = ftell(r);
+        words++;
+        if (wordcmp(fgetword(r), getWord(textToBeFound, 1)))
+        {
+            bool check = true;
+            for (int j = 2; j <= countWords(textToBeFound); j++)
+            {
+                if (!wordcmp(fgetword(r), getWord(textToBeFound, j)))
+                {
+                    check = false;
+                    break;
+                }
+            }
+
+            fseek(r, position, SEEK_SET);
+            fgetword(r);
+
+            if (check && !byword)
+                return position;
+            if (check && byword)
+                return words;
+        }
+    }
+
+    return -1;
+}
+
+struct linkedList * find(char *address, char *textToBeFind, int attributes[4])
+{
+    struct linkedList *output = create_linkedlist();
+
+    FILE *f = fopen(address, "r");
+
+    if (attributes[0])
+    {
+        int count = 0;
+        while (normalFind(f, textToBeFind, false) != -1)
+            count++;
+        fclose(f);
+        addLinkedList(output, count);
+        return output;
+    }
+
+    if (attributes[1])
+    {
+        int count = 1;
+        while (count++ < attributes[1])
+            normalFind(f, textToBeFind, attributes[2]);
+        addLinkedList(output, normalFind(f, textToBeFind, attributes[2]));
+        fclose(f);
+        return output;
+    }
+
+    if (attributes[3])
+    {
+        int answer;
+        while (answer = normalFind(f, textToBeFind, attributes[2]) + 1)
+            addLinkedList(output, answer - 1);
+        fclose(f);
+        return output;
+    }
+
+    addLinkedList(output, normalFind(f, textToBeFind, attributes[2]));
+    fclose(f);
+    return output;
+}
--- phase 1/phase1.c
+++ phase 1/phase1.c
@@ -3,6 +3,8 @@
 #include<stdbool.h>
 #include<string.h>
 
+#include "find.h"
+
 #define CAPACITY 255
 
 struct pos
@@ -43,56 +45,56 @@ int getPos(char *address, struct pos p)
     return position;
 }
 
-int sizeString(char *text)
-{
-    int x = 0;
-    while (1)
-    {
-        if (text[x] == '\0' || text[x] == EOF)
-            return x;
-        x++;
-    }
-}
+//int sizeString(char *text)
+//{
+//    int x = 0;
+//    while (1)
+//    {
+//        if (text[x] == '\0' || text[x] == EOF)
+//            return x;
+//        x++;
+//    }
+//}
 
-char * getWord(char *text, int index)
-{
-    int i = 1;
-    int textPos = 0;
-    //while (text[textPos] == ' ' && text[textPos] == '\n')
-    char *word = (char *) malloc(CAPACITY * sizeof(char));
-    while (1)
-    {
-        if (i == index)
-        {
-            int position = 0;
-            while (text[textPos] != ' ' && text[textPos] != '\0' && text[textPos] != '\n' && text[textPos] != EOF)
-                word[position++] = text[textPos++];
-            textPos++;
-            word[position] = '\0';
-            break;
-        }
+//char * getWord(char *text, int index)
+//{
+//    int i = 1;
+//    int textPos = 0;
+//    //while (text[textPos] == ' ' && text[textPos] == '\n')
+//    char *word = (char *) malloc(CAPACITY * sizeof(char));
+//    while (1)
+//    {
+//        if (i == index)
+//        {
+//            int position = 0;
+//            while (text[textPos] != ' ' && text[textPos] != '\0' && text[textPos] != '\n' && text[textPos] != EOF)
+//                word[position++] = text[textPos++];
+//            textPos++;
+//            word[position] = '\0';
+//            break;
+//        }
+//
+//        while (text[textPos] != ' ' && text[textPos] != '\0' && text[textPos] != '\n' && text[textPos] != EOF)
+//            textPos++;
+//        textPos++;
+//        i++;
+//    }
+//    return word;
+//}
 
-        while (text[textPos] != ' ' && text[textPos] != '\0' && text[textPos] != '\n' && text[textPos] != EOF)
-            textPos++;
-        textPos++;
-        i++;
-    }
-    return word;
-}
-
-char * fgetword(FILE *r, int cap)
-{
-    char *word = (char *) malloc(cap * sizeof(char));
-    char c = fgetc(r);
-    int pos = 0;
-    while (c != ' ' && c != '\n' && c != '\0' && c != EOF && pos < cap)
-    {
-        word[pos++] = c;
-        c = fgetc(r);
-    }
-    word[pos] = '\0';
-    return word;
-}
+//char * fgetword(FILE *r, int cap)
+//{
+//    char *word = (char *) malloc(cap * sizeof(char));
+//    char c = fgetc(r);
+//    int pos = 0;
+//    while (c != ' ' && c != '\n' && c != '\0' && c != EOF && pos < cap)
+//    {
+//        word[pos++] = c;
+//        c = fgetc(r);
+//    }
+//    word[pos] = '\0';
+//    return word;
+//}
 
 void createNewFile(char *address)
 {
@@ -253,246 +255,241 @@ void pastestr(char *address, struct pos p)
     return;
 }
 
-char * correctStar(char *text)
-{
-    char *replacement = (char *) malloc(sizeof(char) * (sizeString(text) + 1));
-    int pos = 0;
-    for (int i = 0; i < sizeString(text); i++)
-    {
-        if (text[i] == '\\' && text[i + 1] == '*')
-        {
-            replacement[pos] = '*';
-            i++;
-        }
-        else
-            replacement[pos] = text[i];
-        pos++;
-    }
-    replacement[pos] = '\0';
-    return replacement;
-}
+//char * correctStar(char *text)
+//{
+//    char *replacement = (char *) malloc(sizeof(char) * (sizeString(text) + 1));
+//    int pos = 0;
+//    for (int i = 0; i < sizeString(text); i++)
+//    {
+//        if (text[i] == '\\' && text[i + 1] == '*')
+//        {
+//            replacement[pos] = '*';
+//            i++;
+//        }
+//        else
+//            replacement[pos] = text[i];
+//        pos++;
+//    }
+//    replacement[pos] = '\0';
+//    return replacement;
+//}
 
-bool wordcmp(char *realWord, char *text)
-{
-    int size = sizeString(text);
-    bool startWild = (text[0] == '*');
-    bool endWild = (text[size - 1] == '*' && text[size - 2] != '\\');
-    char *correctedText = correctStar(text);
-    size = sizeString(correctedText);
+//bool wordcmp(char *realWord, char *text)
+//{
+//    int size = sizeString(text);
+//    bool startWild = (text[0] == '*');
+//    bool endWild = (text[size - 1] == '*' && text[size - 2] != '\\');
+//    char *correctedText = correctStar(text);
+//    size = sizeString(correctedText);
+//
+//    if (!endWild && !startWild) //-------------------------------------case 1
+//        return !strcmp(realWord, correctedText);
+//
+//    if (!startWild && endWild) //--------------------------------------case 2
+//    {
+//        if (sizeString(realWord) < size - 1)
+//            return false;
+//        for (int i = 0; i < size - 1; i++)
+//        {
+//            if (realWord[i] != correctedText[i])
+//                return false;
+//        }
+//        return true;
+//    }
+//
+//    if (startWild && !endWild) //--------------------------------------case 3
+//    {
+//        int wordSize = sizeString(realWord);
+//        if (wordSize < size - 1)
+//            return false;
+//        for (int i = 1; i < size; i++)
+//        {
+//            if (realWord[wordSize - i] != correctedText[size - i])
+//                return false;
+//        }
+//        return true;
+//    }
+//
+//    if (startWild && endWild) //---------------------------------------case 4
+//    {
+//        int wordSize = sizeString(realWord);
+//        if (wordSize < size - 2)
+//            return false;
+//        for (int i = 0; i < wordSize - size + 3; i++)
+//        {
+//            bool isSimilar = true;
+//            for (int pos = 1; pos < size - 1; pos++)
+//            {
+//                if (realWord[i + pos - 1] != correctedText[pos])
+//                {
+//                    isSimilar = false;
+//                    break;
+//                }
+//            }
+//            if (isSimilar)
+//                return true;
+//        }
+//        return false;
+//    }
+//}
 
-    if (!endWild && !startWild) //-------------------------------------case 1
-        return !strcmp(realWord, correctedText);
+//int normalFind(char *address, char *textToBeFound, int from, bool countFirstWord)
+//{
+//    int foundWords = 0;
+//    int foundChars = 0;
+//    int charNum = 0;
+//    int setPos = 0;
+//    FILE *r = fopen(address, "r");
+//    fseek(r, 0, SEEK_END);
+//    int size = ftell(r) - from;
+//    fseek(r, 0, SEEK_SET);
+//
+//    bool found = false;
+//
+//    for (int i = 0; i < from; i++)
+//        fgetc(r);
+//
+//    while (charNum < size)
+//    {
+//        char *tmp = fgetword(r, CAPACITY);
+//        if (wordcmp(tmp, getWord(textToBeFound, foundWords + 1)))
+//            foundChars += sizeString(getWord(textToBeFound, ++foundWords)) + 1;
+//        else
+//            foundChars = foundWords = 0;
+//        charNum += sizeString(tmp) + 1;
+//        if (!foundWords)
+//            setPos = charNum;
+//        if (foundChars >= sizeString(textToBeFound))
+//        {
+//            if (!(!countFirstWord && setPos == 0))
+//            {
+//                found = true;
+//                break;
+//            }
+//        }
+//    }
+//    fclose(r);
+//    if (found)
+//        return setPos;
+//    else
+//        return -1;
+//}
 
-    if (!startWild && endWild) //--------------------------------------case 2
-    {
-        if (sizeString(realWord) < size - 1)
-            return false;
-        for (int i = 0; i < size - 1; i++)
-        {
-            if (realWord[i] != correctedText[i])
-                return false;
-        }
-        return true;
-    }
+//int findByWord(char *address, char *textToBeFound, int from, bool countFirstWord)
+//{
+//    int foundWords = 0;
+//    int foundChars = 0;
+//    int charNum = 0;
+//    int wordNum = 1;
+//    int setPos = 1;
+//    FILE *r = fopen(address, "r");
+//    fseek(r, 0, SEEK_END);
+//    int size = ftell(r) - from;
+//    fseek(r, 0, SEEK_SET);
+//
+//    bool found = false;
+//
+//    for (int i = 0; i < from; i++)
+//        fgetword(r, CAPACITY);
+//
+//    while (charNum < size)
+//    {
+//        char c = fgetc(r);
+//        while (c == ' ')
+//            c = fgetc(r);
+//        fseek(r, -1, SEEK_CUR);
+//        char *tmp = fgetword(r, CAPACITY);
+//        if (wordcmp(tmp, getWord(textToBeFound, foundWords + 1)))
+//            foundChars += sizeString(getWord(textToBeFound, ++foundWords)) + 1;
+//        else
+//            foundChars = foundWords = 0;
+//        charNum += sizeString(tmp) + 1;
+//        wordNum++;
+//        if (!foundWords)
+//            setPos = wordNum;
+//        if (foundChars >= sizeString(textToBeFound))
+//        {
+//            if (countFirstWord || setPos != 1)
+//            {
+//                found = true;
+//                break;
+//            }
+//            else if (setPos == 1)
+//                setPos++;
+//        }
+//    }
+//    fclose(r);
+//    if (found)
+//        return setPos;
+//    else
+//        return -1;
+//}
 
-    if (startWild && !endWild) //--------------------------------------case 3
-    {
-        int wordSize = sizeString(realWord);
-        if (wordSize < size - 1)
-            return false;
-        for (int i = 1; i < size; i++)
-        {
-            if (realWord[wordSize - i] != correctedText[size - i])
-                return false;
-        }
-        return true;
-    }
+//int findByWord(char *address, )
 
-    if (startWild && endWild) //---------------------------------------case 4
-    {
-        int wordSize = sizeString(realWord);
-        if (wordSize < size - 2)
-            return false;
-        for (int i = 0; i < wordSize - size + 3; i++)
-        {
-            bool isSimilar = true;
-            for (int pos = 1; pos < size - 1; pos++)
-            {
-                if (realWord[i + pos - 1] != correctedText[pos])
-                {
-                    isSimilar = false;
-                    break;
-                }
-            }
-            if (isSimilar)
-                return true;
-        }
-        return false;
-    }
-}
-
-int normalFind(char *address, char *textToBeFound, int from, bool countFirstWord)
-{
-    int foundWords = 0;
-    int foundChars = 0;
-    int charNum = 0;
-    int setPos = 0;
-    FILE *r = fopen(address, "r");
-    fseek(r, 0, SEEK_END);
-    int size = ftell(r) - from;
-    fseek(r, 0, SEEK_SET);
-
-    bool found = false;
-
-    for (int i = 0; i < from; i++)
-        fgetc(r);
-
-    while (charNum < size)
-    {
-        char *tmp = fgetword(r, CAPACITY);
-        if (wordcmp(tmp, getWord(textToBeFound, foundWords + 1)))
-            foundChars += sizeString(getWord(textToBeFound, ++foundWords)) + 1;
-        else
-            foundChars = foundWords = 0;
-        charNum += sizeString(tmp) + 1;
-        if (!foundWords)
-            setPos = charNum;
-        if (foundChars >= sizeString(textToBeFound))
-        {
-            if (!(!countFirstWord && setPos == 0))
-            {
-                found = true;
-                break;
-            }
-        }
-    }
-    fclose(r);
-    if (found)
-        return setPos;
-    else
-        return -1;
-}
-
-int findByWord(char *address, char *textToBeFound, int from, bool countFirstWord)
-{
-    int foundWords = 0;
-    int foundChars = 0;
-    int charNum = 0;
-    int wordNum = 1;
-    int setPos = 1;
-    FILE *r = fopen(address, "r");
-    fseek(r, 0, SEEK_END);
-    int size = ftell(r) - from;
-    fseek(r, 0, SEEK_SET);
-
-    bool found = false;
-
-    for (int i = 0; i < from; i++)
-        fgetword(r);
-
-    while (charNum < size)
-    {
-        char c = fgetc(r);
-        while (c == ' ')
-            c = fgetc(r);
-        fseek(r, -1, SEEK_CUR);
-        char *tmp = fgetword(r, CAPACITY);
-        if (wordcmp(tmp, getWord(textToBeFound, foundWords + 1)))
-            foundChars += sizeString(getWord(textToBeFound, ++foundWords)) + 1;
-        else
-            foundChars = foundWords = 0;
-        charNum += sizeString(tmp) + 1;
-        wordNum++;
-        if (!foundWords)
-            setPos = wordNum;
-        if (foundChars >= sizeString(textToBeFound))
-        {
-            if (countFirstWord || setPos != 1)
-            {
-                found = true;
-                break;
-            }
-            else if (setPos == 1)
-                setPos++;
-        }
-    }
-    fclose(r);
-    if (found)
-        return setPos;
-    else
-        return -1;
-}
-
-
-int * find(char *address, char *textToBeFound, int from, int attributes[4])
-{
-    //printf("//opened find\\\\\n");
-
-    int *output;
-    output = (int *)calloc(1, sizeof(int));
-
-    if (!attributes[0] && !attributes[1] && !attributes[2] && !attributes[3])//normal find
-        *output = normalFind(address, textToBeFound, 0, true);
-    else if (!attributes[0] && !attributes[1] && attributes[2] && !attributes[3])//find by word
-        *output = findByWord(address, textToBeFound, 0, true);
-    else if (attributes[0] && !attributes[1] && !attributes[2] && !attributes[3])//count
-    {
-        int x = normalFind(address, textToBeFound, from, !from);
-        if (x == -1)
-            *output = 0;
-        else
-            *output = 1 + *find(address, textToBeFound, x + 1 + from, attributes);
-    }//--------------end of count
-    else if (!attributes[0] && attributes[1] && !attributes[2] && !attributes[3])//find at
-    {
-        int nfrom = from;
-        int x;
-        for (int i = 0; i < attributes[1]; i++)
-        {
-            x = normalFind(address, textToBeFound, nfrom, !nfrom);
-            if (x != -1)
-                nfrom = x + 1 + nfrom;
-            else
-            {
-                nfrom = 0;
-                break;
-            }
-        }
-        *output = nfrom - 1;
-    }//-------end of find at
-    else if (!attributes[0] && attributes[1] && attributes[2] && !attributes[3])//find at by word
-    {
-        int nfrom = from;
-        int wfrom = 0;
-        int y = normalFind(address, textToBeFound, nfrom, !nfrom);
-        for (int i = 0; i < attributes[1]; i++)
-        {
-            int x = normalFind(address, textToBeFound, nfrom, !nfrom);
-            if (x != -1)
-            {
-                int b = findByWord(address, textToBeFound, nfrom, true);
-                printf("//%d\\\\\n", b);
-                wfrom += b;
-                nfrom = x + 1 + nfrom;
-            }
-            else
-            {
-                wfrom = -2;
-                nfrom = 0;
-                break;
-            }
-        }
-            *output = wfrom;
-    }//------end of find at by word
-    else if (!attributes[0] && !attributes[1] && !attributes[2] && attributes[3])//find all
-    {
-        int x = normalFind(address, textToBeFound, from, true);
-        {
-
-        }
-    }
-    return output;
-}
+//int * find(char *address, char *textToBeFound, int from, int attributes[4])
+//{
+//    //printf("//opened find\\\\\n");
+//
+//    int *output;
+//    output = (int *)calloc(1, sizeof(int));
+//
+//    if (!attributes[0] && !attributes[1] && !attributes[2] && !attributes[3])//normal find
+//        *output = normalFind(address, textToBeFound, 0, true);
+//    else if (!attributes[0] && !attributes[1] && attributes[2] && !attributes[3])//find by word
+//        *output = findByWord(address, textToBeFound, 0, true);
+//    else if (attributes[0] && !attributes[1] && !attributes[2] && !attributes[3])//count
+//    {
+//        int x = normalFind(address, textToBeFound, from, !from);
+//        if (x == -1)
+//            *output = 0;
+//        else
+//            *output = 1 + *find(address, textToBeFound, x + 1 + from, attributes);
+//    }//--------------end of count
+//    else if (!attributes[0] && attributes[1] && !attributes[2] && !attributes[3])//find at
+//    {
+//        int nfrom = from;
+//        int x;
+//        for (int i = 0; i < attributes[1]; i++)
+//        {
+//            x = normalFind(address, textToBeFound, nfrom, !nfrom);
+//            if (x != -1)
+//                nfrom = x + 1 + nfrom;
+//            else
+//            {
+//                nfrom = 0;
+//                break;
+//            }
+//        }
+//        *output = nfrom - 1;
+//    }//-------end of find at
+//    else if (!attributes[0] && attributes[1] && attributes[2] && !attributes[3])//find at by word
+//    {
+//        int nfrom = from;
+//        int x;
+//        for (int i = 0; i < attributes[1]; i++)
+//        {
+//            x = findByWord(address, textToBeFound, nfrom, 0);
+//            printf("//%d\\\\\n", x);
+//            if (x != -1)
+//                nfrom += x;
+//            else
+//            {
+//                nfrom = 0;
+//                break;
+//            }
+//        }
+//        *output = nfrom;
+//    }//------end of find at by word
+//    else if (!attributes[0] && !attributes[1] && !attributes[2] && attributes[3])//find all
+//    {
+//        int x = normalFind(address, textToBeFound, from, true);
+//        {
+//
+//        }
+//    }
+//    return output;
+//}
 
 int main()
 {
@@ -533,12 +530,12 @@ int main()
 //    c = fgetword(x, CAPACITY);
 //    printf("%s:-->%d\n", c, sizeString(c));
 
-    int att[4] = {0, 2, 1, 0};
+    int att[4] = {0, 0, 1, 1};
 //    for (int i = 1; i <= 15; i++)
 //        printf("%s\n", getWord("b b b b Salam\nKhobi?\nchert Khobi? chert a a a chert2 chert chert3", i));
     insert("test.txt", start, "b b b b Salam\nKhobi?\nchert Khobi? chert a a chert2 a chert chert3");
     //printf(",,%d,,\n", findByWord("test.txt", "b", 1, true));
-    printf(",,%d,,\n", *find("test.txt", "Khobi?", 0, att));
+    printLinkedList(find("test.txt", "b", att));
 
     //find("test.txt", "hello*", 0);
     return 17;
